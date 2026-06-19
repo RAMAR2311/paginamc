ec8d655d93da166f828bbe45462af4373d7d658d
Marlon Ramirez <marlonramr99@gmail.com>
2026-06-19 16:58:23 -0500

    Mejoras de seguridad: Desactivar debug y bloquear atajos

--- app.py
+++ app.py
@@ -771,4 +771,4 @@ def api_get_users():
 
 if __name__ == '__main__':
     # Ejecuta el servidor de Flask en el puerto 5000
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=False, port=5000, host='0.0.0.0')
